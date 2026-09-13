b14dafccff883b9222f07edc3faabf5e284f44c7
Clinton Gormley <clintongormley@gmail.com>
2026-09-13 19:36:16 +0200

    Add a category colour palette and a readable-text-colour helper
    Signed-off-by: Clinton Gormley <clintongormley@gmail.com>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -61,3 +61,5 @@ export { UrlStateController, type UrlPathConfig } from "./url-state.js";
 export { WtRowActions } from "./components/wt-row-actions.js";
 export { WtTabs, type TabItem } from "./components/wt-tabs.js";
 export { WtCombobox, type ComboboxOption } from "./components/wt-combobox.js";
+
+export { readableTextColor, isHexColor, CATEGORY_PALETTE } from "./category-color.js";
